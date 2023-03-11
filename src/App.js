4135b05d27ca7e194a261af51a8837fa339aa092
restrepo84 <112986175+restrepo84@users.noreply.github.com>
2023-03-11 03:00:01 -0700

    useCallback

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useCallback } from "react";
 
 import Button from "./components/UI/Button/Button";
 import DemoOutput from "./components/Demo/DemoOutput";
@@ -8,9 +8,9 @@ function App() {
   const [showParagraph, setShowParagraph] = useState(false);
   console.log("APP RUNNING");
 
-  const toggleParagraphHandler = () => {
+  const toggleParagraphHandler = useCallback(() => {
     setShowParagraph((prevShowParagraph) => !prevShowParagraph);
-  };
+  }, []);
 
   return (
     <div className="app">
